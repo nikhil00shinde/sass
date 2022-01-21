c8ddb985628b1592d0d12a6dfd810de79392ce4e
Nikhil Shinde <nikhilshinde00@outlook.com>
2022-01-21 14:10:43 +0530

    project structure

--- complete-sass-tutorial/gulpfile.js
+++ complete-sass-tutorial/gulpfile.js
@@ -4,12 +4,13 @@ const sass = require("gulp-sass")(require("sass"));
 // changes sass file to css file
 function buildStyles() {
 	// look for any filename with .scss extension and compile it
-	return src("*.scss").pipe(sass()).pipe(dest("css"));
+	// it will compile all sass file to their css file
+	return src("shinobi/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 // agar change sass file mei kuch change kiya toh css file mei bhi changes reflect kardo
 function watchTask() {
-	watch(["*.scss"], buildStyles);
+	watch(["shinobi/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
